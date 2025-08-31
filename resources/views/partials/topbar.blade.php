7bd5418d82bc3b4d077170e42000fe492860089a
rjjagdish2 <rjjagdish2@gmail.com>
2025-08-31 11:45:24 +0530

    profile_ooption_remove

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -16,11 +16,6 @@
             <!-- Dropdown -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                  aria-labelledby="userDropdown">
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Profile
-                </a>
-                <div class="dropdown-divider"></div>
                 <!-- Logout -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
